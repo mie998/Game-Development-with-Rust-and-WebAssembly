59df942d47a1e42ccf011088d0ae4bdf894afd96
mie998 <nishiwaki.kyoto@gmail.com>
2023-07-29 20:13:14 +0900

    initial  commit

---
+++ walk-the-dog/src/lib.rs
@@ -0,0 +1,51 @@
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::console;
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3]) {
+    let [top, left, right] = points;
+
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(top.0, top.1);
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.close_path();
+    context.stroke();
+}
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    console_error_panic_hook::set_once();
+
+    draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
+
+    // Your code goes here!
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
+}
