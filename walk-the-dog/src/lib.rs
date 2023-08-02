e1830169aed53c113f11debe185f292035855d9d
mie998 <nishiwaki.kyoto@gmail.com>
2023-08-02 23:44:58 +0900

    Added rhb run

--- walk-the-dog/src/lib.rs
+++ walk-the-dog/src/lib.rs
@@ -1,7 +1,15 @@
+use gloo_utils::format::JsValueSerdeExt;
+use rand::prelude::*;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::console;
-use rand::prelude::*;
+
+const RHB_PATH: &str = "walk_the_dog_assets-0.0.7/resized/rhb/";
+const SPRITE_PATH: &str = "walk_the_dog_assets-0.0.7/sprite_sheets/";
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,6 +18,24 @@ use rand::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[derive(Debug, Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+#[derive(Debug, Deserialize)]
+struct Cell {
+    frame: Rect,
+}
 
 fn draw_triangle(
     context: &web_sys::CanvasRenderingContext2d,
@@ -20,7 +46,7 @@ fn draw_triangle(
 
     let color_str = format!("rgb({}, {}, {})", color.0, color.1, color.2);
     context.set_fill_style(&wasm_bindgen::JsValue::from_str(&color_str));
-    
+
     context.move_to(top.0, top.1);
     context.begin_path();
     context.line_to(top.0, top.1);
@@ -71,6 +97,14 @@ fn draw_sierpinski_gasket(context: &web_sys::CanvasRenderingContext2d, points: [
     draw_sierpinski_gasket(context, [top, mid_left, mid_right]);
 }
 
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
+    let window = web_sys::window().unwrap();
+    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
+    let resp: web_sys::Response = resp_value.dyn_into()?;
+
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
+}
+
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -91,10 +125,89 @@ pub fn main_js() -> Result<(), JsValue> {
 
     console_error_panic_hook::set_once();
 
-    let top = (300.0, 0.0);
-    let left = (0.0, 600.0);
-    let right = (600.0, 600.0);
-    draw_sierpinski_gasket(&context, [top, left, right]);
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+
+        let image = web_sys::HtmlImageElement::new().unwrap();
+
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+        let err_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(err_callback.as_ref().unchecked_ref()));
+
+        image.set_src((String::from(RHB_PATH) + "Idle (1).png").as_str());
+
+        success_rx.await;
+
+        context
+            .draw_image_with_html_image_element(&image, 0.0, 0.0)
+            .unwrap();
+
+        let top = (300.0, 0.0);
+        let left = (0.0, 600.0);
+        let right = (600.0, 600.0);
+        draw_sierpinski_gasket(&context, [top, left, right]);
+
+        
+        // read sprite sheet
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
+        let json = fetch_json((String::from(SPRITE_PATH) + "rhb.json").as_str())
+            .await
+            .expect("Failed to fetch JSON");
+
+        let sheet: Sheet = json.into_serde().expect("Failed to parse JSON");
+
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+
+        let image = web_sys::HtmlImageElement::new().unwrap();
+
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+        let err_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(err_callback.as_ref().unchecked_ref()));
+
+        image.set_src((String::from(SPRITE_PATH) + "rhb.png").as_str());
+        success_rx.await;
+
+        let sprite = sheet
+            .frames
+            .get("Run (1).png")
+            .expect("Cell not found");
+        context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+            &image,
+            sprite.frame.x.into(),
+            sprite.frame.y.into(),
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
+            300.0,
+            300.0,
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
+        );
+    });
 
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
