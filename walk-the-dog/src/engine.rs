3eb5b9ff8f1c4f48fba6c16f4d1686359fd3645b
mie998 <nishiwaki.kyoto@gmail.com>
2023-08-06 19:02:23 +0900

    Animation working

--- walk-the-dog/src/engine.rs
+++ walk-the-dog/src/engine.rs
@@ -1,40 +1,97 @@
-use crate::browser;
-use crate::browser::LoopClosure;
-
+use crate::browser::{self, LoopClosure};
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use futures::channel::mpsc::{unbounded, UnboundedReceiver};
-use futures::channel::oneshot::channel;
+use futures::channel::{
+    mpsc::{unbounded, UnboundedReceiver},
+    oneshot::channel,
+};
 use serde::Deserialize;
 use std::{cell::RefCell, collections::HashMap, rc::Rc, sync::Mutex};
-use wasm_bindgen::closure::Closure;
-use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
 use web_sys::{CanvasRenderingContext2d, HtmlImageElement};
 
+#[derive(Deserialize, Clone)]
+pub struct SheetRect {
+    pub x: i16,
+    pub y: i16,
+    pub w: i16,
+    pub h: i16,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct Cell {
+    pub frame: SheetRect,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct Sheet {
+    pub frames: HashMap<String, Cell>,
+}
+
+#[derive(Clone, Copy)]
+pub struct Point {
+    pub x: i16,
+    pub y: i16,
+}
+
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+pub struct Renderer {
+    context: CanvasRenderingContext2d,
+}
+
+impl Renderer {
+    pub fn clear(&self, rect: &Rect) {
+        self.context.clear_rect(
+            rect.x.into(),
+            rect.y.into(),
+            rect.width.into(),
+            rect.height.into(),
+        );
+    }
+
+    pub fn draw_image(&self, image: &HtmlImageElement, frame: &Rect, destination: &Rect) {
+        self.context
+            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                frame.x.into(),
+                frame.y.into(),
+                frame.width.into(),
+                frame.height.into(),
+                destination.x.into(),
+                destination.y.into(),
+                destination.width.into(),
+                destination.height.into(),
+            )
+            .expect("Drawing is throwing exceptions! Unrecoverable error.");
+    }
+}
+
 pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
-    let image = browser::new_image().expect("Failed to create image");
+    let image = browser::new_image()?;
 
     let (complete_tx, complete_rx) = channel::<Result<()>>();
     let success_tx = Rc::new(Mutex::new(Some(complete_tx)));
     let error_tx = Rc::clone(&success_tx);
-
     let success_callback = browser::closure_once(move || {
         if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-            if let Err(err) = success_tx.send(Ok(())) {
-                println!("Could not send successful image loaded message! {:#?}", err);
-            }
+            success_tx.send(Ok(()));
         }
     });
-    let err_callback: Closure<dyn FnMut(JsValue)> = browser::closure_once(move |err| {
+
+    let error_callback: Closure<dyn FnMut(JsValue)> = browser::closure_once(move |err| {
         if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-            if let Err(err) = error_tx.send(Err(anyhow!("Error Loading Image: {:#?}", err))) {
-                println!("Could not send error message on loading image! {:#?}", err);
-            }
+            error_tx.send(Err(anyhow!("Error Loading Image: {:#?}", err)));
         }
     });
 
     image.set_onload(Some(success_callback.as_ref().unchecked_ref()));
-    image.set_onerror(Some(err_callback.as_ref().unchecked_ref()));
+    image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
     image.set_src(source);
 
     complete_rx.await??;
@@ -45,10 +102,11 @@ pub async fn load_image(source: &str) -> Result<HtmlImageElement> {
 #[async_trait(?Send)]
 pub trait Game {
     async fn initialize(&self) -> Result<Box<dyn Game>>;
-    fn update(&mut self, Keystate: &mut KeyState);
+    fn update(&mut self, keystate: &KeyState);
     fn draw(&self, renderer: &Renderer);
 }
 
+// Sixty Frames per second, converted to a frame length in milliseconds
 const FRAME_SIZE: f32 = 1.0 / 60.0 * 1000.0;
 pub struct GameLoop {
     last_frame: f64,
@@ -57,117 +115,44 @@ pub struct GameLoop {
 type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 
 impl GameLoop {
-    pub async fn start(mut game: impl Game + 'static) -> Result<()> {
-        log!("before prepare: {}", browser::now()?);
-        let mut keyevent_reciever = prepare_input()?;
+    pub async fn start(game: impl Game + 'static) -> Result<()> {
+        let mut keyevent_receiver = prepare_input()?;
         let mut game = game.initialize().await?;
-        log!("after prepare: {}", browser::now()?);
+
         let mut game_loop = GameLoop {
             last_frame: browser::now()?,
             accumulated_delta: 0.0,
         };
-        
-        let f: SharedLoopClosure = Rc::new(RefCell::new(None));
-        let g = f.clone();
 
         let renderer = Renderer {
             context: browser::context()?,
         };
 
+        let f: SharedLoopClosure = Rc::new(RefCell::new(None));
+        let g = f.clone();
+
         let mut keystate = KeyState::new();
         *g.borrow_mut() = Some(browser::create_raf_closure(move |perf: f64| {
-            process_input(&mut keystate, &mut keyevent_reciever);
-            
+            process_input(&mut keystate, &mut keyevent_receiver);
+
             game_loop.accumulated_delta += (perf - game_loop.last_frame) as f32;
-            while game_loop.accumulated_delta >= FRAME_SIZE {
-                game.update(&mut keystate);
+            while game_loop.accumulated_delta > FRAME_SIZE {
+                game.update(&keystate);
                 game_loop.accumulated_delta -= FRAME_SIZE;
             }
             game_loop.last_frame = perf;
             game.draw(&renderer);
-            browser::request_animation_frame(f.borrow().as_ref().unwrap())
-                .expect("Failed to request animation frame");
+
+            browser::request_animation_frame(f.borrow().as_ref().unwrap());
         }));
 
         browser::request_animation_frame(
             g.borrow()
                 .as_ref()
-                .ok_or_else(|| anyhow!("No closure found"))?,
+                .ok_or_else(|| anyhow!("GameLoop: Loop is None"))?,
         )?;
-
         Ok(())
     }
-}
-
-pub struct Renderer {
-    context: CanvasRenderingContext2d,
-}
-
-#[derive(Debug, Deserialize)]
-pub struct Rect {
-    pub x: f64,
-    pub y: f64,
-    pub width: f64,
-    pub height: f64,
-}
-
-impl Renderer {
-    pub fn clear(&self, rect: &Rect) {
-        self.context
-            .clear_rect(rect.x, rect.y, rect.width, rect.height);
-    }
-
-    pub fn draw_image(&self, image: &HtmlImageElement, frame: &Rect, dest: &Rect) {
-        self.context
-            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &image,
-                frame.x,
-                frame.y,
-                frame.width,
-                frame.height,
-                dest.x,
-                dest.y,
-                dest.width,
-                dest.height,
-            )
-            .expect("Failed to draw image");
-    }
-}
-
-enum KeyPress {
-    KeyUp(web_sys::KeyboardEvent),
-    KeyDown(web_sys::KeyboardEvent),
-}
-
-fn prepare_input() -> Result<UnboundedReceiver<KeyPress>> {
-    let (keydown_sender, keyevent_receiver) = unbounded();
-    let keydown_sender = Rc::new(RefCell::new(keydown_sender));
-    let keyup_sender = Rc::clone(&keydown_sender);
-    let onkeydown = browser::closure_wrap(Box::new(move |keycode: web_sys::KeyboardEvent| {
-        if let Err(err) = keydown_sender
-            .borrow_mut()
-            .start_send(KeyPress::KeyDown(keycode))
-        {
-            error!("Could not send keyDown message {:#?}", err);
-        }
-    }) as Box<dyn FnMut(web_sys::KeyboardEvent)>);
-
-    let onkeyup = browser::closure_wrap(Box::new(move |keycode: web_sys::KeyboardEvent| {
-        if let Err(err) = keyup_sender
-            .borrow_mut()
-            .start_send(KeyPress::KeyUp(keycode))
-        {
-            error!("Could not send keyUp message {:#?}", err);
-        }
-    }) as Box<dyn FnMut(web_sys::KeyboardEvent)>);
-
-    browser::canvas()?.set_onkeydown(Some(onkeydown.as_ref().unchecked_ref()));
-    browser::canvas()?.set_onkeyup(Some(onkeyup.as_ref().unchecked_ref()));
-
-    onkeyup.forget();
-    onkeydown.forget();
-
-    Ok(keyevent_receiver)
 }
 
 pub struct KeyState {
@@ -176,13 +161,13 @@ pub struct KeyState {
 
 impl KeyState {
     fn new() -> Self {
-        KeyState {
+        return KeyState {
             pressed_keys: HashMap::new(),
-        }
+        };
     }
 
-    pub fn is_pressed(&self, key: &str) -> bool {
-        self.pressed_keys.contains_key(key)
+    pub fn is_pressed(&self, code: &str) -> bool {
+        self.pressed_keys.contains_key(code)
     }
 
     fn set_pressed(&mut self, code: &str, event: web_sys::KeyboardEvent) {
@@ -190,21 +175,48 @@ impl KeyState {
     }
 
     fn set_released(&mut self, code: &str) {
-        self.pressed_keys.remove(code);
+        self.pressed_keys.remove(code.into());
     }
+}
+
+enum KeyPress {
+    KeyUp(web_sys::KeyboardEvent),
+    KeyDown(web_sys::KeyboardEvent),
 }
 
 fn process_input(state: &mut KeyState, keyevent_receiver: &mut UnboundedReceiver<KeyPress>) {
     loop {
         match keyevent_receiver.try_next() {
-            Ok(Some(event)) => match event {
-                KeyPress::KeyUp(event) => state.set_released(&event.code()),
-                KeyPress::KeyDown(event) => state.set_pressed(&event.code(), event),
-            },
             Ok(None) => break,
-            Err(_err) => {
-                break;
-            }
-        }
+            Err(_err) => break,
+            Ok(Some(evt)) => match evt {
+                KeyPress::KeyUp(evt) => state.set_released(&evt.code()),
+                KeyPress::KeyDown(evt) => state.set_pressed(&evt.code(), evt),
+            },
+        };
     }
+}
+
+fn prepare_input() -> Result<UnboundedReceiver<KeyPress>> {
+    let (keydown_sender, keyevent_receiver) = unbounded();
+    let keydown_sender = Rc::new(RefCell::new(keydown_sender));
+    let keyup_sender = Rc::clone(&keydown_sender);
+    let onkeydown = browser::closure_wrap(Box::new(move |keycode: web_sys::KeyboardEvent| {
+        keydown_sender
+            .borrow_mut()
+            .start_send(KeyPress::KeyDown(keycode));
+    }) as Box<dyn FnMut(web_sys::KeyboardEvent)>);
+
+    let onkeyup = browser::closure_wrap(Box::new(move |keycode: web_sys::KeyboardEvent| {
+        keyup_sender
+            .borrow_mut()
+            .start_send(KeyPress::KeyUp(keycode));
+    }) as Box<dyn FnMut(web_sys::KeyboardEvent)>);
+
+    browser::canvas()?.set_onkeydown(Some(onkeydown.as_ref().unchecked_ref()));
+    browser::canvas()?.set_onkeyup(Some(onkeyup.as_ref().unchecked_ref()));
+    onkeydown.forget();
+    onkeyup.forget();
+
+    Ok(keyevent_receiver)
 }
--- walk-the-dog/src/game.rs
+++ walk-the-dog/src/game.rs
@@ -1,129 +1,427 @@
-use crate::browser::fetch_json;
-use crate::engine::{load_image, Game, Rect, Renderer, KeyState};
-use anyhow::Result;
+use self::red_hat_boy_states::*;
+use crate::{
+    browser,
+    engine::{self, Game, KeyState, Rect, Renderer, Sheet},
+};
+use anyhow::{anyhow, Result};
 use async_trait::async_trait;
 use gloo_utils::format::JsValueSerdeExt;
-use serde::Deserialize;
-use std::collections::HashMap;
 use web_sys::HtmlImageElement;
 
-#[derive(Debug, Deserialize)]
-struct SheetRect {
-    x: i16,
-    y: i16,
-    w: i16,
-    h: i16,
+pub struct RedHatBoy {
+    state_machine: RedHatBoyStateMachine,
+    sprite_sheet: Sheet,
+    image: HtmlImageElement,
 }
 
-#[derive(Debug, Deserialize)]
-struct Cell {
-    frame: SheetRect,
+impl RedHatBoy {
+    fn new(sprite_sheet: Sheet, image: HtmlImageElement) -> Self {
+        RedHatBoy {
+            state_machine: RedHatBoyStateMachine::Idle(RedHatBoyState::new()),
+            sprite_sheet,
+            image,
+        }
+    }
+
+    fn run_right(&mut self) {
+        self.state_machine = self.state_machine.transition(Event::Run);
+    }
+
+    fn slide(&mut self) {
+        self.state_machine = self.state_machine.transition(Event::Slide);
+    }
+
+    fn jump(&mut self) {
+        self.state_machine = self.state_machine.transition(Event::Jump);
+    }
+
+    fn update(&mut self) {
+        self.state_machine = self.state_machine.update();
+    }
+
+    fn draw(&self, renderer: &Renderer) {
+        let frame_name = format!(
+            "{} ({}).png",
+            self.state_machine.frame_name(),
+            (self.state_machine.context().frame / 3) + 1
+        );
+        
+        let sprite = self
+            .sprite_sheet
+            .frames
+            .get(&frame_name)
+            .expect("Cell not found");
+
+        renderer.draw_image(
+            &self.image,
+            &Rect {
+                x: sprite.frame.x.into(),
+                y: sprite.frame.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+            &Rect {
+                x: self.state_machine.context().position.x.into(),
+                y: self.state_machine.context().position.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+        );
+    }
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Sheet {
-    frames: HashMap<String, Cell>,
+#[derive(Copy, Clone)]
+enum RedHatBoyStateMachine {
+    Idle(RedHatBoyState<Idle>),
+    Running(RedHatBoyState<Running>),
+    Sliding(RedHatBoyState<Sliding>),
+    Jumping(RedHatBoyState<Jumping>),
 }
 
-#[derive(Debug, Clone, Copy, Deserialize)]
-pub struct Point {
-    pub x: i16,
-    pub y: i16,
+pub enum Event {
+    Run,
+    Jump,
+    Slide,
+    Update,
 }
 
-pub struct WalkTheDog {
-    image: Option<HtmlImageElement>,
-    sheet: Option<Sheet>,
-    frame: u8,
-    position: Point,
+impl RedHatBoyStateMachine {
+    fn transition(self, event: Event) -> Self {
+        match (self, event) {
+            (RedHatBoyStateMachine::Idle(state), Event::Run) => state.run().into(),
+            (RedHatBoyStateMachine::Running(state), Event::Jump) => state.jump().into(),
+            (RedHatBoyStateMachine::Running(state), Event::Slide) => state.slide().into(),
+            (RedHatBoyStateMachine::Idle(state), Event::Update) => state.update().into(),
+            (RedHatBoyStateMachine::Running(state), Event::Update) => state.update().into(),
+            (RedHatBoyStateMachine::Jumping(state), Event::Update) => state.update().into(),
+            (RedHatBoyStateMachine::Sliding(state), Event::Update) => state.update().into(),
+            _ => self,
+        }
+    }
+
+    fn frame_name(&self) -> &str {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => state.frame_name(),
+            RedHatBoyStateMachine::Running(state) => state.frame_name(),
+            RedHatBoyStateMachine::Jumping(state) => state.frame_name(),
+            RedHatBoyStateMachine::Sliding(state) => state.frame_name(),
+        }
+    }
+
+    fn context(&self) -> &RedHatBoyContext {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => state.context(),
+            RedHatBoyStateMachine::Running(state) => state.context(),
+            RedHatBoyStateMachine::Jumping(state) => state.context(),
+            RedHatBoyStateMachine::Sliding(state) => state.context(),
+        }
+    }
+
+    fn update(self) -> Self {
+        self.transition(Event::Update)
+    }
 }
 
-impl WalkTheDog {
-    pub fn new() -> Self {
-        Self {
-            image: None,
-            sheet: None,
-            frame: 0,
-            position: Point { x: 0, y: 0 },
+impl From<RedHatBoyState<Idle>> for RedHatBoyStateMachine {
+    fn from(state: RedHatBoyState<Idle>) -> Self {
+        RedHatBoyStateMachine::Idle(state)
+    }
+}
+
+impl From<RedHatBoyState<Running>> for RedHatBoyStateMachine {
+    fn from(state: RedHatBoyState<Running>) -> Self {
+        RedHatBoyStateMachine::Running(state)
+    }
+}
+
+impl From<RedHatBoyState<Sliding>> for RedHatBoyStateMachine {
+    fn from(state: RedHatBoyState<Sliding>) -> Self {
+        RedHatBoyStateMachine::Sliding(state)
+    }
+}
+
+impl From<RedHatBoyState<Jumping>> for RedHatBoyStateMachine {
+    fn from(state: RedHatBoyState<Jumping>) -> Self {
+        RedHatBoyStateMachine::Jumping(state)
+    }
+}
+
+impl From<SlidingEndState> for RedHatBoyStateMachine {
+    fn from(state: SlidingEndState) -> Self {
+        match state {
+            SlidingEndState::Sliding(sliding) => sliding.into(),
+            SlidingEndState::Running(running) => running.into(),
         }
     }
 }
 
-// const RHB_PATH: &str = "walk_the_dog_assets-0.0.7/resized/rhb/";
+impl From<JumpingEndState> for RedHatBoyStateMachine {
+    fn from(state: JumpingEndState) -> Self {
+        match state {
+            JumpingEndState::Jumping(jumping) => jumping.into(),
+            JumpingEndState::Landing(landing) => landing.into(),
+        }
+    }
+}
+
+mod red_hat_boy_states {
+    use crate::engine::Point;
+
+    const FLOOR: i16 = 475;
+    const IDLE_FRAMES: u8 = 29;
+    const RUNNING_FRAMES: u8 = 23;
+    const JUMPING_FRAMES: u8 = 35;
+    const SLIDING_FRAMES: u8 = 14;
+    const RUNNING_SPEED: i16 = 3;
+    const IDLE_FRAME_NAME: &str = "Idle";
+    const RUN_FRAME_NAME: &str = "Run";
+    const SLIDING_FRAME_NAME: &str = "Slide";
+    const JUMPING_FRAME_NAME: &str = "Jump";
+    const JUMP_SPEED: i16 = -25;
+    const GRAVITY: i16 = 1;
+
+    #[derive(Copy, Clone)]
+    pub struct RedHatBoyState<S> {
+        context: RedHatBoyContext,
+        _state: S,
+    }
+
+    impl<S> RedHatBoyState<S> {
+        pub fn context(&self) -> &RedHatBoyContext {
+            &self.context
+        }
+
+        fn update_context(&mut self, frames: u8) {
+            self.context = self.context.update(frames);
+        }
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct Idle;
+
+    impl RedHatBoyState<Idle> {
+        pub fn new() -> Self {
+            RedHatBoyState {
+                context: RedHatBoyContext {
+                    frame: 0,
+                    position: Point { x: 0, y: FLOOR },
+                    velocity: Point { x: 0, y: 0 },
+                },
+                _state: Idle {},
+            }
+        }
+
+        pub fn frame_name(&self) -> &str {
+            IDLE_FRAME_NAME
+        }
+
+        pub fn update(mut self) -> RedHatBoyState<Idle> {
+            self.update_context(IDLE_FRAMES);
+            self
+        }
+
+        pub fn run(self) -> RedHatBoyState<Running> {
+            RedHatBoyState {
+                context: self.context.reset_frame().run_right(),
+                _state: Running {},
+            }
+        }
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct Running;
+
+    impl RedHatBoyState<Running> {
+        pub fn frame_name(&self) -> &str {
+            RUN_FRAME_NAME
+        }
+
+        pub fn update(mut self) -> RedHatBoyState<Running> {
+            self.update_context(RUNNING_FRAMES);
+            self
+        }
+
+        pub fn jump(self) -> RedHatBoyState<Jumping> {
+            RedHatBoyState {
+                context: self.context.reset_frame().set_vertical_velocity(JUMP_SPEED),
+                _state: Jumping {},
+            }
+        }
+
+        pub fn slide(self) -> RedHatBoyState<Sliding> {
+            RedHatBoyState {
+                context: self.context.reset_frame(),
+                _state: Sliding {},
+            }
+        }
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct Jumping;
+
+    pub enum JumpingEndState {
+        Jumping(RedHatBoyState<Jumping>),
+        Landing(RedHatBoyState<Running>),
+    }
+
+    impl RedHatBoyState<Jumping> {
+        pub fn frame_name(&self) -> &str {
+            JUMPING_FRAME_NAME
+        }
+
+        pub fn update(mut self) -> JumpingEndState {
+            self.update_context(JUMPING_FRAMES);
+
+            if self.context.position.y >= FLOOR {
+                JumpingEndState::Landing(self.land())
+            } else {
+                JumpingEndState::Jumping(self)
+            }
+        }
+
+        pub fn land(self) -> RedHatBoyState<Running> {
+            RedHatBoyState {
+                context: self.context.reset_frame(),
+                _state: Running {},
+            }
+        }
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct Sliding;
+
+    pub enum SlidingEndState {
+        Sliding(RedHatBoyState<Sliding>),
+        Running(RedHatBoyState<Running>),
+    }
+
+    impl RedHatBoyState<Sliding> {
+        pub fn frame_name(&self) -> &str {
+            SLIDING_FRAME_NAME
+        }
+
+        pub fn update(mut self) -> SlidingEndState {
+            self.update_context(SLIDING_FRAMES);
+
+            if self.context.frame >= SLIDING_FRAMES {
+                SlidingEndState::Running(self.stand())
+            } else {
+                SlidingEndState::Sliding(self)
+            }
+        }
+
+        pub fn stand(self) -> RedHatBoyState<Running> {
+            RedHatBoyState {
+                context: self.context.reset_frame(),
+                _state: Running {},
+            }
+        }
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct RedHatBoyContext {
+        pub frame: u8,
+        pub position: Point,
+        velocity: Point,
+    }
+
+    impl RedHatBoyContext {
+        pub fn update(mut self, frame_count: u8) -> Self {
+            self.velocity.y += GRAVITY;
+
+            if self.frame < frame_count {
+                self.frame += 1;
+            } else {
+                self.frame = 0;
+            }
+
+            self.position.x += self.velocity.x;
+            self.position.y += self.velocity.y;
+
+            if self.position.y > FLOOR {
+                self.position.y = FLOOR;
+            }
+
+            self
+        }
+
+        fn reset_frame(mut self) -> Self {
+            self.frame = 0;
+            self
+        }
+
+        fn set_vertical_velocity(mut self, y: i16) -> Self {
+            self.velocity.y = y;
+            self
+        }
+
+        fn run_right(mut self) -> Self {
+            self.velocity.x += RUNNING_SPEED;
+            self
+        }
+    }
+}
+
+pub enum WalkTheDog {
+    Loading,
+    Loaded(RedHatBoy),
+}
+
+impl WalkTheDog {
+    pub fn new() -> Self {
+        WalkTheDog::Loading {}
+    }
+}
+
 const SPRITE_PATH: &str = "walk_the_dog_assets-0.0.7/sprite_sheets/";
 
 #[async_trait(?Send)]
 impl Game for WalkTheDog {
     async fn initialize(&self) -> Result<Box<dyn Game>> {
-        let json = fetch_json((String::from(SPRITE_PATH) + "rhb.json").as_str()).await?;
-        let sheet: Sheet = json.into_serde()?;
-        let image = load_image((String::from(SPRITE_PATH) + "rhb.png").as_str()).await?;
+        match self {
+            WalkTheDog::Loading => {
+                let sheet: Sheet = browser::fetch_json((String::from(SPRITE_PATH) + "rhb.json").as_str())
+                    .await?
+                    .into_serde()?;
 
-        Ok(Box::new(WalkTheDog {
-            image: Some(image),
-            sheet: Some(sheet),
-            frame: self.frame,
-            position: self.position,
-        }))
+                let rhb = RedHatBoy::new(
+                    sheet,
+                    engine::load_image((String::from(SPRITE_PATH) + "rhb.png").as_str()).await?,
+                );
+                Ok(Box::new(WalkTheDog::Loaded(rhb)))
+            }
+            WalkTheDog::Loaded(_) => Err(anyhow!("Error: Game is already initialized")),
+        }
     }
 
-    fn update(&mut self, keystate: &mut KeyState) {
-        if self.frame < 23 {
-            self.frame += 1;
-        } else {
-            self.frame = 0;
-        }
+    fn update(&mut self, keystate: &KeyState) {
+        if let WalkTheDog::Loaded(rhb) = self {
+            if keystate.is_pressed("ArrowRight") {
+                rhb.run_right();
+            }
 
-        // 入力による速度更新
-        let mut velocity = Point { x: 0, y: 0 };
-        if keystate.is_pressed("ArrowRight") {
-            velocity.x += 10;
+            if keystate.is_pressed("ArrowUp") {
+                rhb.jump();
+            }
+
+            if keystate.is_pressed("ArrowDown") {
+                rhb.slide();
+            }
+
+            rhb.update();
         }
-        if keystate.is_pressed("ArrowLeft") {
-            velocity.x -= 10;
-        }
-        if keystate.is_pressed("ArrowUp") {
-            velocity.y -= 10;
-        }
-        if keystate.is_pressed("ArrowDown") {
-            velocity.y += 10;
-        }
-        
-        self.position.x += velocity.x;
-        self.position.y += velocity.y;
     }
 
     fn draw(&self, renderer: &Renderer) {
-        let current_sprite = (self.frame / 3) + 1;
-        let frame_name = format!("Run ({}).png", current_sprite);
-        let sprite = self
-            .sheet
-            .as_ref()
-            .and_then(|sheet| sheet.frames.get(&frame_name))
-            .expect("Cell not found");
-
         renderer.clear(&Rect {
             x: 0.0,
             y: 0.0,
             width: 600.0,
             height: 600.0,
         });
-        self.image.as_ref().map(|image| {
-            renderer.draw_image(
-                &image,
-                &Rect {
-                    x: sprite.frame.x.into(),
-                    y: sprite.frame.y.into(),
-                    width: sprite.frame.w.into(),
-                    height: sprite.frame.h.into(),
-                },
-                &Rect {
-                    x: self.position.x.into(),
-                    y: self.position.y.into(),
-                    width: sprite.frame.w.into(),
-                    height: sprite.frame.h.into(),
-                },
-            );
-        });
+
+        if let WalkTheDog::Loaded(rhb) = self {
+            rhb.draw(renderer);
+        }
     }
 }
